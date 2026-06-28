0b263fef7cd6124f3f7c9f051af89a1736e31a64
fumotto <fumotto@gmail.com>
2026-06-28 11:37:10 +0900

    修正

--- vite/Elo-rating-2/src/vite-env.d.ts
+++ vite/Elo-rating-2/src/vite-env.d.ts
@@ -1,16 +1,5 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
-  readonly VITE_APP_ENV: string;
-  readonly VITE_BASE_PATH: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 declare module '*.css' {
   const content: string;
   export default content;
